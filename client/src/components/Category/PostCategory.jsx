2a1b285eca9d9942797c998a442a0388933aa155
Vivek Kumar <vivek3553.vr@gmail.com>
2024-07-05 13:50:08 +0530

    ui change

--- client/src/components/Category/PostCategory.jsx
+++ client/src/components/Category/PostCategory.jsx
@@ -5,7 +5,7 @@ const PostCategory = ({ categories, onCategorySelect, onClearFilters }) => {
     <div className="flex flex-wrap gap-2 mb-2">
       {/* All Articles Category */}
       <button
-        className="h-10 inline-flex items-center justify-center w-full sm:w-auto text-center py-3 px-4 rounded-full bg-white border border-gray-200 text-sm font-semibold hover:bg-gray-50 focus:ring focus:ring-orange-200 transition duration-200"
+        className="h-10 inline-flex items-center justify-center sm:w-auto text-center py-3 px-4 rounded-full bg-white border border-gray-200 text-sm font-semibold hover:bg-gray-50 focus:ring focus:ring-orange-200 transition duration-200"
         onClick={onClearFilters}
       >
         All Articles
@@ -14,7 +14,7 @@ const PostCategory = ({ categories, onCategorySelect, onClearFilters }) => {
       {categories?.categories?.map((category) => (
         <button
           key={category._id}
-          className="h-10 inline-flex items-center justify-center w-full sm:w-auto text-center py-3 px-4 rounded-full bg-white border border-gray-200 text-sm font-semibold hover:bg-gray-50 focus:ring focus:ring-orange-200 transition duration-200"
+          className="h-10 inline-flex items-center justify-center sm:w-auto text-center py-3 px-4 rounded-full bg-white border border-gray-200 text-sm font-semibold hover:bg-gray-50 focus:ring focus:ring-orange-200 transition duration-200"
           onClick={() => onCategorySelect(category._id)}
         >
           {category.categoryName} ({category.posts?.length})
